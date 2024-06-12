2c18a37bdfdf2ef3a53d3cc435342ee03410b0c0
Jongsung <jos8304@hotmail.com>
2024-06-11 20:38:48 -0400

    2

--- pages/search.js
+++ pages/search.js
@@ -1,15 +1,29 @@
+import ProductList from "@/components/ProductList";
 import SearchForm from "@/components/SearchForm";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Search() {
+  const [products, setProducts] = useState([]);
   const router = useRouter();
   const { q } = router.query;
+
+  async function getProducts() {
+    const res = await axios.get(`/products/?q=${query}`);
+    const nextProducts = res.data.results;
+    setProducts(nextProducts);
+  }
+
+  useEffect(() => {
+    getProducts(q);
+  }, [q]);
 
   return (
     <div>
       <h1>Search page</h1>
       <SearchForm initalValue={q} />
       <h2>{q} 검색 결과</h2>
+      <ProductList products={products} />
     </div>
   );
 }
